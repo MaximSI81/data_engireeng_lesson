be582dfabef9c24ecbe65b86c43a3f7e73608369
Maxim Safonov <safonovmio@gmail.com>
2024-08-21 00:11:54 +0300

    test1

--- test1.py
+++ test1.py
@@ -7,4 +7,4 @@ def standard_deviation(data):
     sd = (n / (len(data) - 1)) ** 0.5
     return round(sd, 2)
 
-print(standard_deviation('1 5 2 7 1 9 3 8 5 9'))
+print(standard_deviation('1 5 2 7 1 9 3 8 5 9 10 11'))
